df475c41ac1b948f2918b1b466fcbbe1a750b949
rahul07sharmars <58891614+rahul07sharmars@users.noreply.github.com>
2021-03-08 17:01:08 +0530

    final result

--- SimpleClock.py
+++ SimpleClock.py
@@ -6,9 +6,14 @@ from time import strftime
 root = Tk()
 root.title("Clock")
 def time():
+    #format of time
     string= strftime("%H:%M:%S %p")
     label.config(text=string)
+    #update it every second
     label.after(1000, time)
-
+#font of the text
+label = Label(root, font=("ds-digital",80),background="black", foreground="cyan")
+label.pack(anchor='center')
+time()
 
 mainloop()
